fe4a3f6c1d9d8af482055874ca182a67fe960936
suzuki86 <tsnr0001@gmail.com>
2014-07-08 01:42:28 +0900

    fix

--- QuickBotTest.php
+++ QuickBotTest.php
@@ -34,7 +34,7 @@ class QuickBotText extends PHPUnit_Framework_TestCase{
   }
 
   public function testRunkit(){
-    runkit_function_redefine('time', 'return "hello";');
+    runkit_function_redefine('time', '', 'return "hello";');
     $this->assertEquals('hello', time());
   }
 }
