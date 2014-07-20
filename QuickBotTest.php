4ade2f840a9bf172110b5e484fe28fbc867c3903
suzuki86 <tsnr0001@gmail.com>
2014-07-20 01:45:08 -0700

    Change

--- QuickBotTest.php
+++ QuickBotTest.php
@@ -8,10 +8,9 @@ class QuickBotText extends PHPUnit_Framework_TestCase{
     $this->assertTrue(mb_send_mail('to@example.com', 'test subject', 'test body'));
     $messages = json_decode(file_get_contents('http://127.0.0.1:1080/messages'));
     $message = json_decode(file_get_contents('http://127.0.0.1:1080/messages/' . $messages[0]->id . '.json'));
-    var_dump($message);
     $this->assertEquals('<to@example.com>', $message->recipients[0]);
     $this->assertEquals('test subject', $message->subject);
     preg_match_all('/\n\n(.*)/', $message->source, $matches);
-    $this->assertEquals('test body', mb_convert_encoding($matches[1][0], 'UTF-8', 'ISO-2022-JP'));
+    $this->assertEquals('test 2body', mb_convert_encoding($matches[1][0], 'UTF-8', 'ISO-2022-JP'));
   }
 }
