595a637cb8b5f98b6f9dc44d2a453cb66f2e64bb
suzuki86 <tsnr0001@gmail.com>
2014-07-19 21:38:41 -0700

    Add tests integrated with mailcatcher

--- QuickBotTest.php
+++ QuickBotTest.php
@@ -3,8 +3,12 @@
 class QuickBotText extends PHPUnit_Framework_TestCase{
 
   public function testSendMail(){
-    $this->assertTrue(mb_send_mail('tsnr0001@gmail.com', 'test', 'body'));
+    $this->assertTrue(mb_send_mail('to@example.com', 'test subject', 'test body'));
     $messages = json_decode(file_get_contents('http://127.0.0.1:1080/messages'));
-    var_dump($messages);
+    $message = json_decode(file_get_contents('http://127.0.0.1:1080/messages/' . $messages[0]->id . '.json'));
+    $this->assertEquals('<to@example.com>', $message->recipients[0]);
+    $this->assertEquals('test subject', $message->subject);
+    preg_match_all('/\n\n(.*)/', $message->source, $matches);
+    $this->assertEquals('test body', $matches[1][0]);
   }
 }
