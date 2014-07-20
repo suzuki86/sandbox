1f1cdacc8f903cf1b763f3eeceb2eeb69000c4f3
suzuki86 <tsnr0001@gmail.com>
2014-07-19 21:10:39 -0700

    Get response form mailcatcher

--- QuickBotTest.php
+++ QuickBotTest.php
@@ -4,5 +4,7 @@ class QuickBotText extends PHPUnit_Framework_TestCase{
 
   public function testSendMail(){
     $this->assertTrue(mb_send_mail('tsnr0001@gmail.com', 'test', 'body'));
+    $messages = json_decode(file_get_contents('http://127.0.0.1:1080/messages'));
+    var_dump($messages);
   }
 }
