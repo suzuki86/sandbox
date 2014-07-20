3be6f90b0cfb6cfbccbf720b7d8520cb027ba33d
suzuki86 <tsnr0001@gmail.com>
2014-07-19 20:55:55 -0700

    Use mailcatcher

--- QuickBotTest.php
+++ QuickBotTest.php
@@ -5,39 +5,6 @@ require_once 'QuickBot.php';
 
 class QuickBotText extends PHPUnit_Framework_TestCase{
 
-  public function testLoadTextFile(){
-    $qb = new QuickBot(new TwitterOAuth('', '', '', ''));
-    $expected = array(
-      'はじめまして',
-      'おはよう',
-      'こんにちは',
-      'こんばんは',
-      'さようなら'
-    );
-    $this->assertSame($expected, $qb->loadTextFile());
-  }
-
-  public function testTweet(){
-    /**
-     * Create TwitterOAuth mock object.
-     */
-    $mockTwitterOAuth = $this->getMockBuilder('TwitteroAuth')
-      ->setConstructorArgs(array('', '', '', ''))
-      ->setMethods(array())
-      ->getMock();
-    $mockTwitterOAuth->expects($this->once())
-      ->method('oAuthRequest')
-      ->will($this->returnValue(true));
-
-    $qb = new QuickBot($mockTwitterOAuth);
-    $qb->tweet();
-  }
-
-  public function testRunkit(){
-    runkit_function_redefine('time', '', 'return "hello";');
-    $this->assertEquals('hello', time());
-  }
-
   public function testSendMail(){
     $this->assertTrue(mb_send_mail('tsnr0001@gmail.com', 'test', 'body'));
   }
